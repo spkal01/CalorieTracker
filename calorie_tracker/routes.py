2bf56eaa43b223d64e0493b46ead384da5725d66
spkal01 <kalligeross@gmail.com>
2025-05-22 02:06:27 +0300

    AdminPanel: Allow admins to change passwords

--- calorie_tracker/routes.py
+++ calorie_tracker/routes.py
@@ -16,6 +16,7 @@ from calorie_tracker import login_manager
 from flask_mail import Message
 from flask_admin.contrib.sqla import ModelView
 from flask_admin import AdminIndexView
+from wtforms import PasswordField
 from calorie_tracker import mail
 import random
 from itsdangerous import URLSafeTimedSerializer
@@ -61,6 +62,20 @@ class User(db.Model, UserMixin):
     is_admin = db.Column(db.Boolean, default=False)
 
 class AdminUser(ModelView):
+
+    column_exclude_list = ['password']
+    form_excluded_columns = ['password']
+
+    form_extra_fields = {
+        'new_password': PasswordField('New Password')
+    }
+
+    def on_model_change(self, form, model, is_created):
+        # If a new password is entered, hash and set it
+        if form.new_password.data:
+            model.password = bcrypt.generate_password_hash(form.new_password.data).decode('utf-8')
+            db.session.commit()
+
     def is_accessible(self):
         return current_user.is_authenticated and current_user.is_admin
 
