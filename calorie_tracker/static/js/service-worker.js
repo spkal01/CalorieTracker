91eaec3272caa227c89ae353d9a254ada17b79c8
spkal01 <kalligeross@gmail.com>
2025-05-24 12:08:46 +0300

    Initial PWA app support

---
+++ calorie_tracker/static/js/service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'calorie-tracker-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/static/css/output.css',
+  '/static/images/favicon.png',
+  // Add other static assets or routes you want cached
+];
+
+// Install event: cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+// Activate event: cleanup old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch event: serve from cache, fallback to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
